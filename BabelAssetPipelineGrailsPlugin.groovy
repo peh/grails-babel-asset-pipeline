5eb2f0ca7ad1e15b1b1fc58de0391e9c9e45d8cd
philipp <philipp@uberall.com>
2015-07-17 16:42:12 +0200

    inital commit

---
+++ BabelAssetPipelineGrailsPlugin.groovy
@@ -0,0 +1,14 @@
+class BabelAssetPipelineGrailsPlugin {
+    // the plugin version
+    def version = "1.0.0"
+    def grailsVersion = "2.2 > *"
+    def title = "React Asset-Pipeline Plugin"
+    def author = "Philipp Eschenbach"
+    def authorEmail = "philipp@errbuddy.net"
+    def description = "Adds babel transformation to Asset-Pipeline."
+    def documentation = "https://github.com/peh/grails-babel-asset-pipeline"
+    def license = "APACHE"
+    def organization = [ name: "errbuddy", url: "https://errbuddy.net/" ]
+    def issueManagement = [ system: "GITHUB", url: "https://github.com/peh/grails-babel-asset-pipeline/issues" ]
+    def scm = [ url: "https://github.com/peh/grails-babel-asset-pipeline" ]
+}
---
+++ test/unit/babel/asset/pipeline/BabelTagLibSpec.groovy
@@ -0,0 +1,20 @@
+package babel.asset.pipeline
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
+ */
+@TestFor(BabelTagLib)
+class BabelTagLibSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
